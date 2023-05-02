2e59502dfa8f730218d5c45e9f351aed62a0babb
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-01 23:23:18 -0500

    Include Context API

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
+import FavoritesContextProvider from './store/context/favorites-context';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -60,43 +61,45 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='MealsCategories'
-          screenOptions={{
-            contentStyle: { backgroundColor: '#3f2f25' },
-            headerStyle: { backgroundColor: '#351401' },
-            headerTintColor: 'white',
-          }}
-        >
-          <Stack.Screen 
-            name='Drawer' 
-            component={DrawerNavigator} 
-            options={{
-              headerShown: false,
+      <FavoritesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName='MealsCategories'
+            screenOptions={{
+              contentStyle: { backgroundColor: '#3f2f25' },
+              headerStyle: { backgroundColor: '#351401' },
+              headerTintColor: 'white',
             }}
-          />
-          <Stack.Screen 
-            name='MealsOverview' 
-            component={MealsOverviewScreen}
-            // We used `useLayoutEffect` in the mealsOverviewScreen to set the title of the screen instead of passing here.
-            // options={({route, navigation}) => {
-            //   const { categoryId } = route.params;
-            //   const selectedCategory = CATEGORIES.find((category) => category.id === categoryId);
-            //   return {
-            //     title: selectedCategory.title,
-            //   }
-            // }}
-          />
-          <Stack.Screen
-            name='MealDetail'
-            component={MealDetailScreen}
-            options={{
-              title: 'About the Meal',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+          >
+            <Stack.Screen 
+              name='Drawer' 
+              component={DrawerNavigator} 
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name='MealsOverview' 
+              component={MealsOverviewScreen}
+              // We used `useLayoutEffect` in the mealsOverviewScreen to set the title of the screen instead of passing here.
+              // options={({route, navigation}) => {
+              //   const { categoryId } = route.params;
+              //   const selectedCategory = CATEGORIES.find((category) => category.id === categoryId);
+              //   return {
+              //     title: selectedCategory.title,
+              //   }
+              // }}
+            />
+            <Stack.Screen
+              name='MealDetail'
+              component={MealDetailScreen}
+              options={{
+                title: 'About the Meal',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
     </>
   );
 }
